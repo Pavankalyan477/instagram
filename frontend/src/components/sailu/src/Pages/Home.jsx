ac58ff2c567470a31f5b4ecb088ae5e09c3483cb
sailuc <sailuchandragiri2000@gmail.com>
2021-12-18 14:11:14 +0530

    files added

--- frontend/src/components/sailu/src/Pages/Home.jsx
+++ frontend/src/components/sailu/src/Pages/Home.jsx
@@ -17,8 +17,8 @@ export const Home = () => {
           <div className="right">
               <div className="up">
                   <div className="insta_title"></div>
-                  <Box className="phonenumber">
-                      <TextField   margin="dense" variant="outlined" label="Phonenumber, username or email"><button>pavan</button></TextField>
+                  <Box >
+                      <TextField  className="phonenumber" margin="dense" variant="outlined" label="Phonenumber, username or email"><button>pavan</button></TextField>
                   </Box>
                   <Box>
                       <TextField  className="password" margin="dense" variant="outlined" label="Password"></TextField>
